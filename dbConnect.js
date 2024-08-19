e7a2d66fee15f18a6f1c13bde2042366a30163e3
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-19 23:27:37 +0530

    db name issue

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 
 const connectToDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    // const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
     console.log(
       `\n MongoDB connection !! DB HOST:${connectionInstance.connection.host}`
     );
