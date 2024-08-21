651aff50564626b84d863ef88f24ceaa8a89132c
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-21 15:57:49 +0530

    unecessary file removed.

--- routes/userRouter.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { addUser } from "../controllers/userController.js";
-const router = express.Router();
-router.route("/add-user").post(addUser);
-
-export default router;
