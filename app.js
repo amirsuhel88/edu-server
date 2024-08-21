8d84753a980c6f846d9a5f35dde4326f13ca2e72
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-21 16:05:17 +0530

    user controller structre added. need to enhance.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import user from "./routes/userRouter.js";
 import auth from "./routes/authRouter.js";
 const app = express();
 
@@ -14,7 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //router connection
-app.use("/api/v1", user);
 app.use("/api/v2", auth);
 
 export default app;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { addUser } from "../controllers/userController.js";
 const router = express.Router();
-router.route("/add-user").post(addUser);
 
 export default router;
